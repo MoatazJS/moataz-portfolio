5223eb581dd084c638367c14414169844692643e
Moataz <moataz_abdullah@outlook.com>
2025-10-27 20:18:20 +0300

    creating sections in the hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,14 +7,22 @@ export default function Hero() {
         id="Hero"
         className="bg-slate-950 min-h-screen flex justify-center items-center "
       >
-        <div className="mx-auto container flex flex-col items-center justify-center text-slate-100  md:flex md:flex-row md:justify-around md:items-center ">
-          <div className="grid grid-cols-1">
+        <div className="mx-auto container flex flex-col items-center justify-center text-slate-100 md:flex md:flex-row md:justify-around md:items-center ">
+          <div className="grid grid-cols-1 text-center md:text-start">
             <h1 className="font-bold text-slate-300 text-xl md:text-3xl lg:text-4xl">
               Hi, I’m Moataz.
               <br />
-              <span className="text-blue-400"> I'm a Frontend Developer!</span>
+              <span className="text-blue-400">
+                {" "}
+                I&apos;m a Frontend Developer!
+              </span>
             </h1>
-            <p></p>
+            <p className="text-slate-300 text-lg mt-2 mx-4 md:mx-0">
+              I develop web applications with a blend of clean design and
+              efficient code.
+            </p>
+            <div>logos</div>
+            <div>buttons</div>
           </div>
           <div>dada</div>
         </div>
