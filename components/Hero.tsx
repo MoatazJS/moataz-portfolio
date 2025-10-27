0dc27731bb7f5c516b42721451679755d70e1bf5
Moataz <moataz_abdullah@outlook.com>
2025-10-27 20:03:49 +0300

    creating layout of hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,12 +3,22 @@ import React from "react";
 export default function Hero() {
   return (
     <>
-      <div className="min-h-screen text-center flex justify-center align-center text-white bg-black">
-        <h1>YOOOOOOO</h1>
-        <button className="py-10 px-10 text-white bg-yellow-400">
-          HELLO BUTTON
-        </button>
-      </div>
+      <section
+        id="Hero"
+        className="bg-slate-950 min-h-screen flex justify-center items-center "
+      >
+        <div className="mx-auto container flex flex-col items-center justify-center text-slate-100  md:flex md:flex-row md:justify-around md:items-center ">
+          <div className="grid grid-cols-1">
+            <h1 className="font-bold text-slate-300 text-xl md:text-3xl lg:text-4xl">
+              Hi, I’m Moataz.
+              <br />
+              <span className="text-blue-400"> I'm a Frontend Developer!</span>
+            </h1>
+            <p></p>
+          </div>
+          <div>dada</div>
+        </div>
+      </section>
     </>
   );
 }
