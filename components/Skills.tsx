d7bed7832ce9ab66c7b7d856e35de92c93ddc921
Moataz <moataz_abdullah@outlook.com>
2025-10-31 18:50:50 +0200

    finished 5th card

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { Code2Icon, PaletteIcon, ZapIcon } from "lucide-react";
+import { Code2Icon, PaletteIcon, ZapIcon, BrainCircuit } from "lucide-react";
 import { easeInOut, motion } from "framer-motion";
 export default function Skills() {
   return (
@@ -128,6 +128,37 @@ export default function Skills() {
             >
               <p className="text-sm md:text-[16px]">
                 Redux, Jotai, React Query
+              </p>
+            </motion.div>
+          </div>
+          {/* 5th card */}
+          <div className="bg-linear-to-b md:bg-linear-to-t from-slate-800 rounded-lg hover:border-indigo-600  flex flex-col justify-center items-center text-center p-5">
+            <motion.div
+              initial={{ opacity: 0, y: 15 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1.3, ease: easeInOut }}
+              viewport={{ once: true, amount: 0.2 }}
+            >
+              <BrainCircuit className="w-9 h-9 md:w-12 md:h-12" />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, y: 10 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1.3, ease: easeInOut }}
+              viewport={{ once: true, amount: 0.2 }}
+            >
+              <h1 className="text-xl md:text-2xl bg-linear-to-r from-indigo-400 to-blue-500 bg-clip-text text-transparent">
+                Modern Tools
+              </h1>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 1.3, ease: easeInOut }}
+              viewport={{ once: true, amount: 0.2 }}
+            >
+              <p className="text-sm md:text-[16px]">
+                Git, Vite, Vercel, Postman
               </p>
             </motion.div>
           </div>
