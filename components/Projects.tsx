e6d1ed19cbd44acd4650f9e8b18c9731abfe6f94
Moataz <moataz_abdullah@outlook.com>
2025-11-02 19:11:10 +0200

    finished section layout of projects seciton

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,22 @@
+import Image from "next/image";
 import React from "react";
 
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <>
+      <section className="min-h-screen container mx-auto flex flex-col items-center justify-center gap-10">
+        <h1 className="mb-8 text-2xl md:text-4xl bg-linear-to-r from-indigo-400 to-blue-500 bg-clip-text text-transparent">
+          Featured Projects
+        </h1>
+        <div className="hidden md:grid md:grid-cols-3 align-middle gap-6 text-slate-50">
+          <div>hi1</div>
+          <div>hi1</div>
+          <div>hi1</div>
+          <div>hi1</div>
+          <div>hi1</div>
+          <div>hi1</div>
+        </div>
+      </section>
+    </>
+  );
 }
